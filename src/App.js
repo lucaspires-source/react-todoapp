dbb9f05ff41093afee091e81f6dbf34b4be406f5
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-01-15 08:15:04 -0300

    basic styling and structure done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import './App.css'
 const App = () => {
   return (
-    <div>
-      
+    <div className="App">
+      <header>
+          <h1>Lucas's Todo List</h1>
+      </header>
     </div>
   )
 }
